5b80b89f1c50ed96eeed88f24d73d784bc1fb18e
Sander Hahn <sanderhahn@gmail.com>
2024-04-21 13:49:13 +0200

    Don't strech background on a non-desktop ratio

--- helpers/Page.tsx
+++ helpers/Page.tsx
@@ -7,7 +7,7 @@ export function Page({ children }: { children: ComponentChildren }) {
       <img
         src="/background.png"
         alt="bg"
-        class="absolute top-0 left-0 w-full min-h-screen -z-10 overflow-hidden"
+        class="absolute top-0 left-0 bg-center min-h-screen -z-10 overflow-hidden"
       />
       <div class="flex flex-col justify-center items-center w-full h-screen children:(bg-[#F9F9F9] border-1 border-gray-300)">
         {children}
